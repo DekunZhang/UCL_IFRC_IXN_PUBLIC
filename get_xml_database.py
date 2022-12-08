47517149fe11dda6ebc60374027c615e902faa1f
Dekun Zhang <dekun.zhang.20@ucl.ac.uk>
2022-12-08 13:29:17 +0000

    feat: change saving directory and make code more robust

---
+++ get_xml_database.py
@@ -0,0 +1,23 @@
+import requests
+import time
+import os
+from GLOBAL_VARS import countries
+
+
+base_url = "https://www.desinventar.net/DesInventar/download/DI_export_"
+
+if __name__ == '__main__':
+    directory = "./XMLdatabase"
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    for i, (code, country) in enumerate(countries):
+        new_url = base_url + code + ".zip"
+        filename = f"{directory}/{code}_{country}.zip"
+        print(i)
+        print(country)
+        if os.path.exists(filename):
+            continue
+        r = requests.get(new_url)
+        with open(filename, 'wb') as f:
+            f.write(r.content)
+        time.sleep(1)
