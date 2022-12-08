01e74fa6ee05d26e758cd7cf5cc018343fee5c8e
Dekun Zhang <dekun.zhang.20@ucl.ac.uk>
2022-12-08 14:41:25 +0000

    chore: change directory and country codes/names

---
+++ deprecated/data_cleanning_deprecated.py
@@ -0,0 +1,35 @@
+from XMLdatabase_downloader.GLOBAL_VARS import countries
+import os
+
+
+def clean_file(filepath: str):
+    with open(filepath, 'r') as f:
+        try:
+            contents = f.readlines()
+        except UnicodeDecodeError as e:
+            return []
+    return contents[5:-2]
+
+
+def main():
+    source_directory = "./CSVdatabases"
+    assert os.path.exists(source_directory)
+    target_directory = "./cleaned_output"
+    if not os.path.exists(target_directory):
+        os.makedirs(target_directory)
+    for code, country in countries:
+        source_folder = f"{source_directory}/{code}_{country}"
+        assert os.path.exists(source_folder)
+        target_folder = f"{target_directory}/{code}_{country}"
+        if not os.path.exists(target_folder):
+            os.makedirs(target_folder)
+        with os.scandir(source_folder) as it:
+            for source_file in it:
+                cleaned_contents = clean_file(source_file.path)
+                target_file = source_file.path.replace("CSVdatabases", "cleaned_output")
+                with open(target_file, "w") as tf:
+                    tf.writelines(cleaned_contents)
+
+
+if __name__ == '__main__':
+    main()
